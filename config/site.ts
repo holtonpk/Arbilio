3ae1a774b94a9b6cbdeaf459442a508c9cecc51e
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2023-05-12 18:40:34 -0600

    env site url

--- config/site.ts
+++ config/site.ts
@@ -4,8 +4,7 @@ export const siteConfig: SiteConfig = {
   name: "TikDrop.io",
   description:
     "An open source application built using the new router, server components and everything new in Next.js 13.",
-  url: "https://drop-shipping-czyih59h3-holtonpk.vercel.app",
-  // url: process.env.NEXT_PUBLIC_SITE_URL as string,
+  url: process.env.NEXT_PUBLIC_SITE_URL as string,
   ogImage: "https://tx.shadcn.com/og.jpg",
   links: {
     twitter: "https://twitter.com/shadcn",
