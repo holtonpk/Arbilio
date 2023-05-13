c815273a92b0f8a0da95ca4a33e81c33ca500e34
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2023-05-12 18:18:35 -0600

    upgrades to pro

--- pages/api/accountDatabase.ts
+++ pages/api/accountDatabase.ts
@@ -1,70 +1,82 @@
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import { db } from "@/context/Auth";
+// import {
+//   doc,
+//   collection,
+//   getDocs,
+//   query,
+//   limit,
+//   getDoc,
+//   where,
+// } from "firebase/firestore";
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<any>
+// ) {
+//   const collectionRef = collection(db, "tiktokAccounts");
+//   const q = query(collectionRef, limit(8), where("topPosts", "!=", []));
+//   const docs = await getDocs(q);
+
+//   const formattedData = docs.docs.map(async (_doc) => {
+//     const record = _doc.data();
+
+//     let productData = null;
+//     let topPostsData = null;
+
+//     if (record.product) {
+//       const productRef = doc(db, "tiktokProducts", record.product);
+//       const productInfo = await getDoc(productRef);
+//       productData = productInfo.data();
+//     }
+
+//     if (record.topPosts && record.topPosts.length) {
+//       let topPosts = record.topPosts.map(async (post: any) => {
+//         const postRef = doc(db, "tiktokPosts", post);
+//         const postData = await getDoc(postRef);
+//         return postData.data();
+//       });
+//       topPostsData = await Promise.all(topPosts);
+//     }
+
+//     return {
+//       recordId: record.id,
+//       id: record.userInfo.user?.id,
+//       uniqueId: record.uniqueId,
+//       nickname: record.userInfo.user?.nickname,
+//       stats: {
+//         likes: record.userInfo.stats.heartCount,
+//         followers: record.userInfo.stats.followerCount,
+//         following: record.userInfo.stats.followingCount,
+//         posts: record.userInfo.stats.videoCount,
+//       },
+//       avatar: record.avatar,
+//       secUid: record.userInfo.user.secUid,
+//       bio: record.userInfo.user.signature,
+//       bioLink: record.userInfo.user.bioLink?.link
+//         ? record.userInfo.user.bioLink.link
+//         : null,
+//       userInfo: record.userInfo,
+//       posts: record.posts,
+//       topPosts: topPostsData,
+//       product: productData,
+//     };
+//   });
+
+//   const data = await Promise.all(formattedData);
+
+//   res.status(200).json(data);
+// }
+
 import type { NextApiRequest, NextApiResponse } from "next";
-import { db } from "@/context/Auth";
-import {
-  doc,
-  collection,
-  getDocs,
-  query,
-  limit,
-  getDoc,
-  where,
-} from "firebase/firestore";
 
-export default async function handler(
+type ResponseData = {
+  message: string;
+};
+
+export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<any>
+  res: NextApiResponse<ResponseData>
 ) {
-  const collectionRef = collection(db, "tiktokAccounts");
-  const q = query(collectionRef, limit(8), where("topPosts", "!=", []));
-  const docs = await getDocs(q);
-
-  const formattedData = docs.docs.map(async (_doc) => {
-    const record = _doc.data();
-
-    let productData = null;
-    let topPostsData = null;
-
-    if (record.product) {
-      const productRef = doc(db, "tiktokProducts", record.product);
-      const productInfo = await getDoc(productRef);
-      productData = productInfo.data();
-    }
-
-    if (record.topPosts && record.topPosts.length) {
-      let topPosts = record.topPosts.map(async (post: any) => {
-        const postRef = doc(db, "tiktokPosts", post);
-        const postData = await getDoc(postRef);
-        return postData.data();
-      });
-      topPostsData = await Promise.all(topPosts);
-    }
-
-    return {
-      recordId: record.id,
-      id: record.userInfo.user?.id,
-      uniqueId: record.uniqueId,
-      nickname: record.userInfo.user?.nickname,
-      stats: {
-        likes: record.userInfo.stats.heartCount,
-        followers: record.userInfo.stats.followerCount,
-        following: record.userInfo.stats.followingCount,
-        posts: record.userInfo.stats.videoCount,
-      },
-      avatar: record.avatar,
-      secUid: record.userInfo.user.secUid,
-      bio: record.userInfo.user.signature,
-      bioLink: record.userInfo.user.bioLink?.link
-        ? record.userInfo.user.bioLink.link
-        : null,
-      userInfo: record.userInfo,
-      posts: record.posts,
-      topPosts: topPostsData,
-      product: productData,
-    };
-  });
-
-  const data = await Promise.all(formattedData);
-
-  res.status(200).json(data);
+  res.status(200).json({ message: "Hello from Next.js!" });
 }
-
--- pages/api/top-accounts.ts
+++ pages/api/top-accounts.ts
@@ -1,28 +1,41 @@
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import { db } from "@/context/Auth";
+// import { collection, query, getDocs } from "firebase/firestore";
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<any>
+// ) {
+//   const collectionRef = collection(db, "tiktokAccounts");
+//   const q = query(collectionRef);
+//   const docs = await getDocs(q);
+
+//   const formattedData = docs.docs.map(async (_doc) => {
+//     const record = _doc.data();
+
+//     return {
+//       recordId: record.id,
+//       id: record.userInfo.user?.id,
+//       uniqueId: record.uniqueId,
+//       nickname: record.userInfo.user?.nickname,
+//       accountStats: record.accountStats,
+//       avatar: record.avatar,
+//     };
+//   });
+
+//   const data = await Promise.all(formattedData);
+//   res.status(200).json(data);
+// }
+
 import type { NextApiRequest, NextApiResponse } from "next";
-import { db } from "@/context/Auth";
-import { collection, query, getDocs } from "firebase/firestore";
 
-export default async function handler(
+type ResponseData = {
+  message: string;
+};
+
+export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<any>
+  res: NextApiResponse<ResponseData>
 ) {
-  const collectionRef = collection(db, "tiktokAccounts");
-  const q = query(collectionRef);
-  const docs = await getDocs(q);
-
-  const formattedData = docs.docs.map(async (_doc) => {
-    const record = _doc.data();
-
-    return {
-      recordId: record.id,
-      id: record.userInfo.user?.id,
-      uniqueId: record.uniqueId,
-      nickname: record.userInfo.user?.nickname,
-      accountStats: record.accountStats,
-      avatar: record.avatar,
-    };
-  });
-
-  const data = await Promise.all(formattedData);
-  res.status(200).json(data);
+  res.status(200).json({ message: "Hello from Next.js!" });
 }
