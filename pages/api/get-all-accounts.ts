53f4ac65ecc0d64b091f884789fc1bb1773a6d71
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2023-06-06 16:56:40 -0600

    static param test 4

--- pages/api/get-all-accounts.ts
+++ pages/api/get-all-accounts.ts
@@ -10,31 +10,32 @@ import {
   where,
 } from "firebase/firestore";
 
+import { storage } from "@/config/data-storage";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any[]>
 ) {
-  //   const q = query(
-  //     collection(db, storage.accounts),
-  //     where("userInfo", "!=", null),
-  //     limit(5)
-  //   );
-  //   const docs = await getDocs(q);
+  const q = query(
+    collection(db, storage.accounts),
+    where("userInfo", "!=", null)
+  );
+  const docs = await getDocs(q);
 
-  //   const formattedData = docs.docs.map(async (_doc) => {
-  //     const record = _doc.data();
-  //     return {
-  //       id: record.id,
-  //     };
-  //   });
+  const formattedData = docs.docs.map(async (_doc) => {
+    const record = _doc.data();
+    return {
+      id: record.id,
+    };
+  });
 
-  //   const data = (await Promise.all(formattedData)) as AccountDataType[];
-  const data = [
-    { id: "7208887380605699077" },
-    { id: "7211952463829795882" },
-    { id: "7167278845606462465" },
-    { id: "7206197740939756590" },
-  ];
+  const data = (await Promise.all(formattedData)) as any[];
+  // const data = [
+  //   { id: "7208887380605699077" },
+  //   { id: "7211952463829795882" },
+  //   { id: "7167278845606462465" },
+  //   { id: "7206197740939756590" },
+  // ];
 
   res.status(200).json(data);
 }
